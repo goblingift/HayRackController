9895bd322e19d5e900acb9f30a26129d05d98831
André Keßler <andre.kessler91@gmx.de>
2018-09-16 14:06:46 +0200

    Initial commit of the spring boot application

---
+++ HayRackController/src/main/java/gift/goblin/HayRackController/HayRackControllerApplication.java
@@ -0,0 +1,12 @@
+package gift.goblin.HayRackController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HayRackControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HayRackControllerApplication.class, args);
+	}
+}
