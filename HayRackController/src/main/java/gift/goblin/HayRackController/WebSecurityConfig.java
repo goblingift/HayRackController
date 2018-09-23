cb1641018c024143a9088e9cf01876ca6e40dae3
André Keßler <andre.kessler91@gmx.de>
2018-09-23 20:00:25 +0200

    Added h2-console to allowed path

--- HayRackController/src/main/java/gift/goblin/HayRackController/WebSecurityConfig.java
+++ HayRackController/src/main/java/gift/goblin/HayRackController/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/resources/**", "/registration").permitAll()
+                .antMatchers("/resources/**", "/registration", "/h2-console").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
