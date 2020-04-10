f1b6d2e80bd2513941daa92223725e8e02b5bc15
André Keßler <andre.kessler91@gmx.de>
2020-04-10 22:57:33 +0200

    Modularized Navigation and header includes
    Created thymeleaf fragments for the JS/CSS includes and embedded the whole nav-bar into another fragment.

--- HayRackController/src/main/java/gift/goblin/HayRackController/controller/WeightController.java
+++ HayRackController/src/main/java/gift/goblin/HayRackController/controller/WeightController.java
@@ -4,26 +4,34 @@
  */
 package gift.goblin.HayRackController.controller;
 
+import gift.goblin.HayRackController.service.io.WebcamDeviceService;
 import gift.goblin.HayRackController.service.io.dto.TemperatureAndHumidity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Controller for the weight overview page.
+ *
  * @author andre
  */
 @Controller
 public class WeightController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-    
+
+    @Autowired
+    private WebcamDeviceService webcamService;
+
     @GetMapping(value = "/weight")
     public String renderWeightOverview(Model model) {
-
+        
+        model.addAttribute("webcam_count", webcamService.getWebcamCount());
+        
         return "weight";
     }
-    
+
 }
